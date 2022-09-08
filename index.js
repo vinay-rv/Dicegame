7b1c093a098d74b874a428a7fd82c1aa03feacc5
Vinay R V <112092286+vinay-rv@users.noreply.github.com>
2022-09-08 21:40:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ if(randonNumber1>randomNumber2){
 }else if(randonNumber1<randomNumber2){
     document.querySelector("h1").innerHTML="Player 2 Wins";
 }else{
-    document.querySelector("h1").innerHTML="Draw";
+    document.querySelector("h1").innerHTML="Ties";
 };
 }
